257491abe4fdb1614fb0dec8cd798b5777efdce3
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-11-18 16:53:20 +0530

    Add files via upload

---
+++ handlers/alive.py
@@ -0,0 +1,38 @@
+# https://t.me/legendaditya
+
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_message(command(["start", f"start@DevilTrishaRoBot"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/af3dad3866909b93b036b.png",
+        caption=f"""
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 Jøɩɳ Ɦɘɤɘ & Sʋƥƥøɤʈ 💞", url=f"https://t.me/JaiHindChatting")
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(command(["help", f"help@DevilTrishaRoBot"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/af3dad3866909b93b036b.png",
+        caption=f"""
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 Ƈɭɩƈƙ Ɦɘɤɘ Føɤ Ɦɘɭƥ 💞", url=f"https://t.me/JaiHindChatting")
+                ]
+            ]
+        ),
+    )
