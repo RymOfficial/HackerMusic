c8b4758bc5005de825262bdec7fc6ed111c8fd40
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-11-20 08:57:37 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,12 +7,12 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID", "8236321"))
-API_HASH = getenv("API_HASH", "974c0347e3de1ba02b540f07fb970e55")
-ARQ_API_KEY = getenv("ARQ_API_KEY", "FQRTDU-SYSNFN-TZKITE-ZNNMMS-ARQ")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+ARQ_API_KEY = getenv("ARQ_API_KEY")
 BOT_TOKEN = getenv("BOT_TOKEN")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "900"))
-SESSION_NAME = getenv("SESSION_NAME", "STRING_SESSION")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT"))
+SESSION_NAME = getenv("SESSION_NAME", "session")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1323020756").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 aiohttpsession = aiohttp.ClientSession()
