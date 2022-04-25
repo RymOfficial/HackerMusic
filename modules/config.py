b10172179bbabf0b6148b17218f08ed95e1becaa
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2022-04-25 10:09:36 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,13 +7,13 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID", "14621169"))
-API_HASH = getenv("API_HASH", "61e480d82ff09acbf967ccea4fa2884f")
-BOT_USERNAME = getenv("BOT_USERNAME", "TrishaMusicBot")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
 ARQ_API_KEY = getenv("ARQ_API_KEY")
-BOT_TOKEN = getenv("BOT_TOKEN", "5323995137:AAHCiJXcHNJgKzB5cWGLBQLZFIOGNYam98Y")
+BOT_TOKEN = getenv("BOT_TOKEN")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-SESSION_NAME = getenv("SESSION_NAME", "BQBYyPf85xxq079gAA6zPIFnvuIe7cc4EqXg-U0f49BHPxNO9NGRyzZD1pIP19HXF_jpiMCPRHa0VmxNSJ4uFe9K-dUiVr3QmBP805taV79MfSP7I7utB5supuLcxUSEz-4anu2NDIU9vbLA2ctwOVceXYU9NXJcOAPimZeXlDuZmyY03tmTR56PjcqNR3dA5wPM9TgaqptDde95xiO1UP360qt8xAbbR2YBFtWbn7P8Otqv0PoDCjOUUTrRReBRQckfZs-jNvgN_Uawtd-3GLEjnTTv-kcFAiqXaGr6Rd1aaJjlX7GX3hYjvi0Kdi9Y7KrotFYC_qpuohxRyWIFA7mwAAAAAS1N7eYA")
+SESSION_NAME = getenv("SESSION_NAME")
 YOUR_NAME = getenv("https://telegra.ph/file/8fddb775d567de8a63940.jpg")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5265780945").split()))
